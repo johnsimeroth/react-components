f4c864f124c70e45952b0cc87721171a346e610c
Johnathan Simeroth <john.simeroth@gmail.com>
2023-02-21 12:08:38 -0800

    solo sprint complete

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// TODO
+const {useState} = React;
+
+const GroceryListItem = ({item}) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const style = {fontWeight: isHovering ? 'bold' : 'normal'}
+  return (
+    <li style={style} onMouseEnter={() => setIsHovering(!isHovering)} onMouseLeave={() => setIsHovering(!isHovering)}>
+      {item}
+    </li>
+  );
+};
+
+const GroceryList = ({items}) => (
+  <div>
+    <ul>
+      {items.map((item) => (
+        <GroceryListItem item={item} />
+      ))}
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList items={['Potatoes', 'Cheese Curds', 'SunnyD']}/>, document.getElementById("app"))
